eb31657af9869c402d48b49b9f6f62796a337ea6
lupitavaldovinos <gvaldovinos-molina.sp@mysummitps.org>
2016-01-29 23:46:52 +0000

    update

--- 3rd semester project/planner.js
+++ 3rd semester project/planner.js
@@ -1,8 +1,13 @@
 var form = document.getElementById('myform'),
     field = document.getElementById('newitem'),
     todo = document.getElementById('planner'),
-    options = document.getElementById('options');
-    
+    options = document.getElementById('options'),
+    grocerySection = document.getElementById('grocery'),
+    sportSection = document.getElementById('sports'),
+    schoolSection = document.getElementById('school'),
+    dropdown = document.getElementById('dropdown'),
+    dropdown2 = document.getElementById('dropdown2');    
+
 form.addEventListener( 'submit', function(evt) {
   var text = field.value;
   if(dropdown.value == "underline"){
@@ -20,22 +25,69 @@ form.addEventListener( 'submit', function(evt) {
   evt.preventDefault();
 }, false);
 
+
 todo.addEventListener( 'click', function(evt) {
   var tar = evt.target;
   
   if(tar.style.textDecoration === "line-through"){
-   tar.parentNode.removeChild(tar);
-   } 
-   else{
+  tar.parentNode.removeChild(tar);
+  } 
+  else{
      tar.style.textDecoration = "line-through";
-   }
+  }
   evt.preventDefault();
 }, false)
 
 
+form.addEventListener('submit', function(evt){
+
+var text = field.value;
+if(dropdown2.value == "grocery"){
+    grocerySection.innerHTML += '<p>' + text + '</p>'; 
+}
+else if(dropdown2.value == "sports"){
+    sportSection.innerHTML += '<p>' + text + '</p>';
+}
+else if(dropdown2.value == "school"){
+    schoolSection.innerHTML += '<p>' + text + '</p>';
+}
+  evt.preventDefault();
+}, false);
 
 
+grocerySection.addEventListener( 'click', function(evt) {
+  var tar = evt.target;
+  
+  if(tar.style.textDecoration === "line-through"){
+  tar.parentNode.removeChild(tar);
+  } 
+  else{
+     tar.style.textDecoration = "line-through";
+  }
+  evt.preventDefault();
+}, false)
+
+schoolSection.addEventListener( 'click', function(evt) {
+  var tar = evt.target;
+  
+  if(tar.style.textDecoration === "line-through"){
+  tar.parentNode.removeChild(tar);
+  } 
+  else{
+     tar.style.textDecoration = "line-through";
+  }
+  evt.preventDefault();
+}, false)
 
 
-
-
+sportSection.addEventListener( 'click', function(evt) {
+  var tar = evt.target;
+  
+  if(tar.style.textDecoration === "line-through"){
+  tar.parentNode.removeChild(tar);
+  } 
+  else{
+     tar.style.textDecoration = "line-through";
+  }
+  evt.preventDefault();
+}, false)
